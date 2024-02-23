12f1a9b1835f43209e3b8afb59b542741b08a485
Squid O <5jing235@naver.com>
2024-02-23 20:18:11 +0900

    0223_벽 생성
    0223_벽 생성

---
+++ Source/Tiva/Private/buildWallActor_OJS.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "buildWallActor_OJS.h"
+
+// Sets default values
+AbuildWallActor_OJS::AbuildWallActor_OJS()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AbuildWallActor_OJS::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AbuildWallActor_OJS::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
