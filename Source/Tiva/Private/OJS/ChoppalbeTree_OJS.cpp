7e3925bda5bc427a49aee962b24378120bed52ea
Squid O <5jing235@naver.com>
2024-02-27 21:34:10 +0900

    0227_나무자르기
    0227_나무자르기

---
+++ Source/Tiva/Private/OJS/ChoppalbeTree_OJS.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OJS/ChoppalbeTree_OJS.h"
+
+// Sets default values
+AChoppalbeTree_OJS::AChoppalbeTree_OJS()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AChoppalbeTree_OJS::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AChoppalbeTree_OJS::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
