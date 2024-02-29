5bcd3a4b3347afc61534465d6e329e50fffeaf5c
Squid O <5jing235@naver.com>
2024-02-29 15:32:18 +0900

    0209_정완님 요청_target 기둥 추가
    0209_정완님 요청_target 기둥 추가

---
+++ Source/Tiva/Private/OJS/houseTargetColumn.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OJS/houseTargetColumn.h"
+
+// Sets default values
+AhouseTargetColumn::AhouseTargetColumn()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AhouseTargetColumn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AhouseTargetColumn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
