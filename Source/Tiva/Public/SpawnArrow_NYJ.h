0481c9a0c7da2ef8f3821c0204613a08cd0ae43d
yunju6528@duksung.ac.kr <yunju6528@duksung.ac.kr>
2024-03-07 20:33:34 +0900

    몬스터 데미지 연결 완료/ 확인은 안했지만 될 듯!!

--- Source/Tiva/Public/SpawnArrow_NYJ.h
+++ Source/Tiva/Public/SpawnArrow_NYJ.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -22,5 +22,7 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+
 
 };
