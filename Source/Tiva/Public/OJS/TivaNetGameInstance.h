a10cd0f3be23a9dc4ce091687cabfd35c65048ee
Squid O <5jing235@naver.com>
2024-03-19 21:26:25 +0900

    퇴근전 푸시(네트워크진행)
    퇴근전 푸시(네트워크진행)

---
+++ Source/Tiva/Public/OJS/TivaNetGameInstance.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "TivaNetGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TIVA_API UTivaNetGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Init() override;
+
+	IOnlineSessionPtr sessionInterface;
+
+	void CreateRoom( int32 maxPlayerCount , FString roomName );
+
+	void OnMyCreateRoom( FName sessionName , bool bWasSuccessful );
+
+	FString hostName = TEXT( "hello" );
+	//FString roomName;
+};
--- Source/Tiva/Tiva.Build.cs
+++ Source/Tiva/Tiva.Build.cs
@@ -8,6 +8,6 @@ public class Tiva : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG","NavigationSystem", "AIModule", "GameplayTasks", "Niagara" , "OnlineSubsystem" ,"OnlineSubsystemSteam"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG","NavigationSystem", "AIModule", "GameplayTasks", "Niagara" , "OnlineSubsystem", "OnlineSubsystemSteam" });
 	}
 }
